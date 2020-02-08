26b27170c1aaa556dd8387fdde13d053f7502e37
anomalocation <k.keita.0801@gmail.com>
2020-02-08 15:45:03 +0900

    add face api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request, abort
-
+from io import BytesIO
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -8,14 +10,17 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage
 )
 
 app = Flask(__name__)
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
 YOUR_CHANNEL_SECRET = os.getenv('YOUR_CHANNEL_SECRET')
+YOUR_FACE_API_KEY = os.environ["YOUR_FACE_API_KEY"]
+YOUR_FACE_API_ENDPOINT = os.environ["YOUR_FACE_API_ENDPOINT"]
 
+face_client = FaceClient(YOUR_FACE_API_ENDPOINT, CognitiveServicesCredentials(YOUR_FACE_API_KEY))
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
@@ -45,6 +50,35 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    try:
+        # メッセージIDを受け取る
+        message_id = event.message.id
+        # メッセージIDに含まれるmessage_contentを抽出する
+        message_content = line_bot_api.get_message_content(message_id)
+        # contentの画像データをバイナリデータとして扱えるようにする
+        image = BytesIO(message_content.content)
+
+        # Detect from streamで顔検出
+        detected_faces = face_client.face.detect_with_stream(image)
+        # 検出結果に応じて処理を分ける
+        if detected_faces != []:
+            # 検出された顔の最初のIDを取得
+            text = detected_faces[0].face_id
+        else:
+            # 検出されない場合のメッセージ
+            text = "no faces detected"
+    except:
+        # エラー処理
+        text = "error"
+
+    # LINEチャネルを通じてメッセージを返答
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=text)
+    )
 
 if __name__ == "__main__":
     app.run()
