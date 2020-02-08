1173a872a8b21024f37fddeaf51b7179d2d1f4d3
anomalocation <k.keita.0801@gmail.com>
2020-02-08 16:39:16 +0900

    add face verify

--- app.py
+++ app.py
@@ -19,10 +19,13 @@ YOUR_CHANNEL_ACCESS_TOKEN = os.getenv('YOUR_CHANNEL_ACCESS_TOKEN')
 YOUR_CHANNEL_SECRET = os.getenv('YOUR_CHANNEL_SECRET')
 YOUR_FACE_API_KEY = os.environ["YOUR_FACE_API_KEY"]
 YOUR_FACE_API_ENDPOINT = os.environ["YOUR_FACE_API_ENDPOINT"]
+PERSON_GROUP_ID = os.getenv('PERSON_GROUP_ID')
+PERSON_ID_AUDREY = os.getenv('PERSON_ID_AUDREY')
 
 face_client = FaceClient(YOUR_FACE_API_ENDPOINT, CognitiveServicesCredentials(YOUR_FACE_API_KEY))
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
 
 
 @app.route("/callback", methods=['POST'])
@@ -67,12 +70,27 @@ def handle_image(event):
         if detected_faces != []:
             # 検出された顔の最初のIDを取得
             text = detected_faces[0].face_id
+
+            # 顔検出ができたら顔認証を行う
+            valified = face_client.face.verify_face_to_person(
+                face_id = detected_faces[0].face_id,
+                person_group_id = PERSON_GROUP_ID,
+                person_id = PERSON_ID_AUDREY
+            )
+            # 認証結果に応じて処理を変える
+            if valified:
+                if valified.is_identical:
+                    # 顔認証が一致した場合（スコアもつけて返す）
+                    text = 'この写真はオードリーヘップバーンです(score:{:.3f})'.format(valified.confidence)
+                else:
+                    # 顔認証が一致した場合（スコアもつけて返す）
+                    text = 'この写真はオードリーヘップバーンではありません(score:{:.3f})'.format(valified.confidence)
         else:
             # 検出されない場合のメッセージ
-            text = "no faces detected"
+            text = "写真から顔が検出できませんでした。他の画像で試してください。"
     except:
         # エラー処理
-        text = "error"
+        text = "エラーが発生しました。"
 
     # LINEチャネルを通じてメッセージを返答
     line_bot_api.reply_message(
